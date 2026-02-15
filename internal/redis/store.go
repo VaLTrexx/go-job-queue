9c7268483669286e5e3be801d16512d6680b6c64
Abhinav Paste <valtrexcode01@gmail.com>
2026-02-15 18:17:37 +0530

    Redis Store implementation complete 3/3

--- internal/redis/store.go
+++ internal/redis/store.go
@@ -26,3 +26,38 @@ func (s *RedisStore) Save(j job.Job) error {
 
 	return s.red_client.HSet(context.Background(), "jobs", j.ID, data).Err()
 }
+
+func (s *RedisStore) Get(id string) (job.Job, bool, error) {
+	val, err := s.red_client.HGet(context.Background(), "jobs", id).Result()
+
+	if err == r.Nil {
+		return job.Job{}, false, nil
+	}
+	if err != nil {
+		return job.Job{}, false, err
+	}
+	var j job.Job
+	if err := json.Unmarshal([]byte(val), &j); err != nil {
+		return job.Job{}, false, err
+	}
+
+	return j, true, nil
+}
+
+func (s *RedisStore) All() ([]job.Job, error) {
+	values, err := s.red_client.HGetAll(context.Background(), "jobs").Result()
+	if err != nil {
+		return nil, err
+	}
+
+	jobs := make([]job.Job, 0, len(values))
+	for _, val := range values {
+		var j job.Job
+		if err := json.Unmarshal([]byte(val), &j); err != nil {
+			return nil, err
+		}
+		jobs = append(jobs, j)
+	}
+
+	return jobs, nil
+}
